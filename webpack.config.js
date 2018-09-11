dc29ab0c8c0f845b0bc8d89137a212bab4c8489c
Dobos <cristina.dobos@garmin.com>
2018-09-11 14:27:44 +0300

    remove package-lock.json and unused devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
